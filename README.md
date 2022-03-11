1a99027b1a1c679f2a9e68e5ca4e5b1614ded699
Nick Fan <nfan@tyro.com>
2022-03-11 21:43:29 +1100

    First commit for compose spike

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nft-wallet
+
+### Query all the assets 
+#https://cardano-mainnet.blockfrost.io/api/v0/accounts/stake1u90wxdrwaxu6muz8qhagx6hd238gq3u2pssmtg2p3dzqr3qxw67fg/addresses/assets
+
+### Get IPFS assets 
+#https://cardano-mainnet.blockfrost.io/api/v0/assets/c56d4cceb8a8550534968e1bf165137ca41e908d2d780cc1402079bd4368696c6c65644b6f6e6736323533
+
+### Query the IPFS link url 
+#https://ipfs.io/ipfs/QmZnok2nKfmTthxm2v2GK3Y9fEUGgdp6Rd3ckBGEAKdmeg
+
+
---
+++ app/src/main/java/com/example/jetpackproject/ui/NftListFragment.kt
@@ -0,0 +1,36 @@
+package com.example.jetpackproject.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class NftListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                hello()
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun hello() {
+        Text("hello world")
+    }
+}
